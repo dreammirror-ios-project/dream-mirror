2d4ac487eeec719df44bd11c6d2e05dca9688dfe
Maxim Tarakanov <mstarakanov@yandex.ru>
2021-11-15 15:36:58 +0300

    gitattr

--- DreamMirror/DreamMirror/Setup/ModuleBuilder.swift
+++ DreamMirror/DreamMirror/Setup/ModuleBuilder.swift
@@ -1,16 +1,8 @@
 //
-//  AuthPresenter.swift
+//  ModuleBuilder.swift
 //  DreamMirror
 //
 //  Created by Максим Тараканов on 15.11.2021.
 //
 
 import Foundation
-
-protocol AuthPresenterProtocol {
-    
-}
-
-protocol AuthViewProtocol {
-    
-}
